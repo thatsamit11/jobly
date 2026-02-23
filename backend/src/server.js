61232b38b7286b445e1ae8897d9a3db06031d2b5
Amit Thakur <amitthakur91107@gmail.com>
2026-02-24 00:51:04 +0530

    add files

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,15 @@ const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://jobly-9jla.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
