3eca45b2195d24eb199e74caddc66c60e0fb957e
Amit Thakur <amitthakur91107@gmail.com>
2026-02-23 21:56:56 +0530

    add files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home/Home";
+import Home from "./pages/home/home";
 import Auth from "./pages/auth/Auth";
 
 // ================= CANDIDATE =================
